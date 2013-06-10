9a5f014a2ea08f6c9745299731eea6d8c4c37041
Lawrence Davis <lawrence@kohactive.com>
2013-06-10 14:48:42 -0500

    Setup rspec

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in locationator.gemspec
 gemspec
+gem "rspec"
---
+++ spec/locationator_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Locationator do
+  it "lat should return a float" do
+    pending
+  end
+end
