da960ea76c115cf0cea49b83724b1ae2ac43b2a2
Lawrence Davis <lawrence@kohactive.com>
2013-06-11 13:50:07 -0500

    Write pending method tests

--- spec/locationator_spec.rb
+++ spec/locationator_spec.rb
@@ -1,7 +1,51 @@
 require 'spec_helper'
 
 describe Locationator do
-  it "lat should return a float" do
-    pending
+  describe ".lat" do
+    it "should return a float" do
+      pending
+    end
+  end
+
+  describe ".lng" do
+    it "should return a float" do
+      pending
+    end
+  end
+
+  describe ".lat_lng" do
+    it "should return an array with 2 floats" do
+      pending
+    end
+  end
+
+  describe ".zip" do
+    it "should return a string" do
+      pending
+    end
+  end
+
+  describe ".format" do
+    it "should return a string" do
+      pending
+    end
+  end
+
+  describe ".ip_lat" do
+    it "should return a float" do
+      pending
+    end
+  end
+
+  describe ".ip_lng" do
+    it "should return a float" do
+      pending
+    end
+  end
+
+  describe ".ip_lat_lng" do
+    it "should return an array with 2 floats" do
+      pending
+    end
   end
 end
