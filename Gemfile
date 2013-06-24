41bddfb7f4dcdfd4d7ebf1801bd251b3fba94969
Lawrence Davis <lawrence@kohactive.com>
2013-06-24 15:05:03 -0500

    Write tests for all methods

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in locationator.gemspec
 gemspec
 gem "rspec"
+gem "factory_girl"
+gem "shoulda"
--- spec/locationator_spec.rb
+++ spec/locationator_spec.rb
@@ -1,51 +1,94 @@
 require 'spec_helper'
 
 describe Locationator do
+
+  before :each do
+    @location = "800 W Huron St Chicago, IL"
+  end
+
   describe ".lat" do
     it "should return a float" do
-      pending
+      Locationator.lat(@location).should be_a(Float)
     end
   end
 
   describe ".lng" do
     it "should return a float" do
-      pending
+      Locationator.lng(@location).should be_a(Float)
     end
   end
 
   describe ".lat_lng" do
-    it "should return an array with 2 floats" do
-      pending
+    it "should return an array" do
+      Locationator.lat_lng(@location).should be_a(Array)
+    end
+
+    it "should return an array with 2 values" do
+      Locationator.lat_lng(@location).length.should eq(2)
+    end
+
+    it "should return an array with a float for the first value" do
+      location1 = Locationator.lat_lng(@location).first
+      location1.should be_a(Float)
+    end
+
+    it "should return an array with a float for the second value" do
+      location2 = Locationator.lat_lng(@location).last
+      location2.should be_a(Float)
     end
   end
 
   describe ".zip" do
     it "should return a string" do
-      pending
+      Locationator.zip(@location).should be_a(String)
+    end
+
+    it "should return a string that is can be converted to an integer" do
+      location = Locationator.zip(@location)
+      location.to_i.should be_a(Integer)
+    end
+
+    it "should be 5 digits long" do
+      location = Locationator.zip(@location)
+      location.length.should eq(5)
     end
   end
 
   describe ".format" do
     it "should return a string" do
-      pending
+      Locationator.format(@location).should be_a(String)
     end
   end
 
   describe ".ip_lat" do
     it "should return a float" do
-      pending
+      Locationator.ip_lat.should be_a(Float)
     end
   end
 
   describe ".ip_lng" do
     it "should return a float" do
-      pending
+      Locationator.ip_lng.should be_a(Float)
     end
   end
 
   describe ".ip_lat_lng" do
-    it "should return an array with 2 floats" do
-      pending
+    it "should return an array" do
+      Locationator.ip_lat_lng.should be_a(Array)
+    end
+
+    it "should return an array with 2 values" do
+      Locationator.ip_lat_lng.length.should eq(2)
+    end
+
+    it "should return an array with a float for the first value" do
+      location1 = Locationator.ip_lat_lng.first
+      location1.should be_a(Float)
+    end
+
+    it "should return an array with a float for the second value" do
+      location2 = Locationator.ip_lat_lng.last
+      location2.should be_a(Float)
     end
   end
 end
